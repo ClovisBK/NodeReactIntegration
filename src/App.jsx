6c8a448795819f7cd677af6ba10bcffa1eab6922
Kebeh Clovis Bin <kebehclovisbin@gmail.com>
2025-11-10 12:27:33 +0100

    add reusable Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './index.css'
 import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import Navbar from './Components/Navbar';
 
 const Home = () => <h2>Welcome to the Card system</h2>;
 const Login = () => <h2>Login Page</h2>;
@@ -10,11 +11,11 @@ const Register = () => <h2>Registration Page</h2>;
 const App = () => {
   return (
     <Router>
-      <nav style={{padding: "1rem", background: "#eee"}}>
+      <Navbar style={{padding: "1rem", background: "#eee"}}>
           <Link to="/">Home</Link>
           <Link to="/login" style={{margin: "0 10px"}}>Login</Link>
           <Link to="/register">Register</Link>
-      </nav>
+      </Navbar>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/login' element={<Login/>}/>
