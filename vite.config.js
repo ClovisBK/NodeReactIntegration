6fda859dea6c658ff618f6c7bb847df755ddde92
Kebeh Clovis Bin <kebehclovisbin@gmail.com>
2025-11-14 11:51:51 +0100

    Removed the proxy from vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/V1/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-      },
-    },
-  },
 });
