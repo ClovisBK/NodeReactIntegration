dba38e4232e09105d5a2ab4e25ec78df4262d166
Kebeh Clovis Bin <kebehclovisbin@gmail.com>
2025-11-14 10:42:27 +0100

    added the url from env in the api.js file

--- src/api/api.js
+++ src/api/api.js
@@ -1,10 +1,5 @@
 import axios from "axios";
 
-const baseURL =
-  process.env.NODE_ENV === "production"
-    ? "https://jwtauthenticationnodejs-production.up.railway.app/V1/api"
-    : "/V1/api"; // local dev uses proxy
-
 const api = axios.create({
   baseURL:import.meta.evn.VITE_API_URL || "/V1/api",
   withCredentials: true,
